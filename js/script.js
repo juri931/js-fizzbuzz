1f39ed2c0d83a6275ad0c0f8707ffac29afc05c3
juri931 <impa.mat@hotmail.it>
2024-02-22 16:17:50 +0100

    [add] js file step 1

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// 1. Creazione del contenitore
+let container = document.createElement('div');
+container.className = 'container';
+document.body.appendChild(container);
+
+console.log(container);
+
