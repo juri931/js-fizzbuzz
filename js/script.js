09cad1674c012be9dae2e94d92c46bd7b2b854b6
juri931 <impa.mat@hotmail.it>
2024-02-22 16:29:55 +0100

    [add] step 2

--- js/script.js
+++ js/script.js
@@ -5,3 +5,11 @@ document.body.appendChild(container);
 
 console.log(container);
 
+// 2. Ciclo for
+for (let i = 1; i <= 100; i++) {
+  let output = "";
+  if (i % 3 === 0) output += "Fizz";
+  if (i % 5 === 0) output += "Buzz";
+  if (output === "") output = i;
+
+}
